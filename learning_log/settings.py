91844b33e09b66be758387d36a63aa8b2da80d36
kerokeropyu <sharkchu7@gmail.com>
2023-09-16 21:35:33 +0900

    Topic, EntryのCRUD、ユーザーログイン画面の作成

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # 自分のアプリケーション
     'learning_logs',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
